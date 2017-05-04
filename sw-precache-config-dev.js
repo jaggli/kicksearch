733a670832c5994d1cb2be7d52e6512badce705b
Dave Steck <david.steck@scout24.ch>
2017-05-04 22:23:48 +0200

    precache config
    Signed-off-by: Dave Steck <david.steck@scout24.ch>

--- sw-precache-config-dev.js
+++ sw-precache-config-dev.js
@@ -6,5 +6,9 @@ module.exports = {
     'public/static/**/!(*map*)'
   ],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
-  swFilePath: 'public/service-worker.js'
+  swFilePath: 'public/service-worker.js',
+  runtimeCaching: [{
+    urlPattern: /^https:\/\/kicksearch-data\.herokuapp\.com\/api\/v1/,
+    handler: 'fastest'
+  }]
 }
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -6,5 +6,9 @@ module.exports = {
     'build/static/**/!(*map*)'
   ],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
-  swFilePath: 'build/service-worker.js'
+  swFilePath: 'build/service-worker.js',
+  runtimeCaching: [{
+    urlPattern: /^https:\/\/kicksearch-data\.herokuapp\.com\/api\/v1/,
+    handler: 'fastest'
+  }]
 }
